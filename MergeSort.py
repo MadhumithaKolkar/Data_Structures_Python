801ce63cd246aa72c8ccfaa9c875af13e8df2af1
Madhumitha <madhumithakolkar@gmail.com>
2023-10-24 17:54:41 +0530

    Adding Merge Sort Code

---
+++ MergeSort.py
@@ -0,0 +1,43 @@
+def merge_sort(arr):
+    if len(arr)<=1:
+        return
+
+    mid = len(arr)//2
+
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    merge_two_sorted_lists(left,right,arr)
+
+def merge_two_sorted_lists(a,b,arr):
+    print("This is a",a)
+    print("This is b", b)
+    len_a = len(a)
+    len_b = len(b)
+
+    i = j = k = 0
+
+    while i<len_a and j <len_b:
+        if a[i] <= b[j]:
+            arr[k] = a[i]
+            i+=1
+        else:
+            arr[k] = b[j]
+            j += 1
+        k +=1
+    while i<len_a:
+        arr[k] = a[i]
+        i+=1
+        k+=1
+    while j<len_b:
+        arr[k] = b[j]
+        j+=1
+        k+=1
+
+if __name__=='__main__':
+    arr = [5,1,76,9,4,1,765,3432]
+    merge_sort(arr)
+    print(arr)
