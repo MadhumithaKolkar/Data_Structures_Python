26396737ece4238e1f749027fc2dcb9b7d2e1e33
Madhumitha <madhumithakolkar@gmail.com>
2024-10-06 01:37:24 +0530

    Recursion

--- Prac/Rec.py
+++ Prac/Rec.py
@@ -1,8 +1,65 @@
-def dummy(num,count):
-    print(f"In iter : {count}")
-    if count == 10:
-        return f"   The value of the number now is {num}"
-    return dummy(num*5,count+1)
+def print_name(name, i, n):
+    if i >= n:
+        print()
+        return
+    print(name)
+    print_name(name, i + 1, n)
+    return
 
-print(dummy(5,1))
-print(5**10)
+
+def print_number(i, n):
+    if i > n:
+        print()
+        return
+    print(n)
+    print_number(i + 1, n)
+
+
+def rev_print(i, n):
+    if i <= 0:
+        print()
+        return
+    print(i)
+    rev_print(i - 1, n)
+    return
+
+
+def backtrack_nums(i,n):
+    if i<1:
+        return
+    backtrack_nums(i-1,n)
+    print(i)
+
+def backtrack_reverse(i,n):
+    if i>n:
+        return
+    backtrack_reverse(i+1,n)
+    print(i)
+
+def param_nums(i,sum):
+    if i < 0:
+        print(sum)
+        return
+    return param_nums(i-1 , sum+i)
+
+def funct_rec(n):
+    if n == 0 :
+        return 0
+    return n + funct_rec(n-1)
+
+def factorial(n):
+    if n==0:
+        return 1
+    return n*factorial(n-1)
+
+
+print_name("Striver", 0, 3)
+print_number(1 , 5)
+rev_print(5,5)
+print("BT\n")
+backtrack_nums(5,5)
+print("\n")
+backtrack_reverse(1,5)
+param_nums(3 , 0)
+print(funct_rec(3))
+print(factorial(3))
